9378de1305d81708e42e6a99fb57b9e6ac6d47c0
yuenwu1996@gms.tku.edu.tw <yuenwu1996@gms.tku.edu.tw>
2019-09-19 10:23:20 +0800

    回到頂端

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
-// jqready+tab
-// 按下回到頂端按鈕時回到頂端
-// click
-// animate 動畫 fadeIn
-// scrollTop 元素的上方
-// jqReady
-$(document).ready(function () {
-    // jqClick
-    $('back-to-top').click(function (e) { 
-        // 找到 html, body 執行動畫 ({動畫屬性: 值}, 時間);
+// 文件 準備完成 執行 () 內的東西
+// 自訂方法：function 方法名稱() {  }
+// 匿名方法：function () { }
+// JS API 彈出訊息：alert ("訊息");
+$(document).ready(function(){
+  
+  // 回到頂部
+      $('#back-to-top').click(function (e) { 
+  // 找到 html, body 執行動畫 ({動畫屬性: 值}, 時間);
         $('html, body').animate({scrollTop: 0}, 1000);
-        
-    });
-});
+      });
+  });
