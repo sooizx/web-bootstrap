4ccca149c66a49b3691194080030f47fd7610a5d
yuenwu1996@gms.tku.edu.tw <yuenwu1996@gms.tku.edu.tw>
2019-09-25 19:38:30 +0800

    商品縮圖顯現

--- js/main.js
+++ js/main.js
@@ -27,5 +27,20 @@ $(document).ready(function () {
       $('#back-to-top').fadeOut(800);
     }
   });
+  // productbox
+  $(function () {
+    // 用來顯示大圖片用
+    var $showImage = $('#show-image');
+
+    // 當滑鼠移到 .abgne-block-20120106 中的某一個超連結時
+    $('.abgne-block-20120106 a').mouseover(function () {
+      // 把 #show-image 的 src 改成被移到的超連結的位置
+      $showImage.attr('src', $(this).attr('href'));
+    }).click(function () {
+      // 如果超連結被點擊時, 取消連結動作
+      return false;
+    });
+  });
 
 });
+
